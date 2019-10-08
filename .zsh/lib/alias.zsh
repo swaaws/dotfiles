6655a1053bd982cb89ed0873c980eefdfef2a807
swaaws <40453962+swaaws@users.noreply.github.com>
2019-10-08 02:57:45 +0200

    Update alias.zsh

--- .zsh/lib/alias.zsh
+++ .zsh/lib/alias.zsh
@@ -1,4 +1,4 @@
-alias update_dotfiles='`curl -fsSL https://raw.githubusercontent.com/swaaws/dotfiles/master/dotfiles.sh | sh`'
+alias update_dotfiles="curl -fsSL https://raw.githubusercontent.com/swaaws/dotfiles/master/dotfiles.sh | sh"
 alias ll='ls -la'
 alias lh='ls -lah'
 alias wan4='dig @resolver1.opendns.com A myip.opendns.com +short -4'
