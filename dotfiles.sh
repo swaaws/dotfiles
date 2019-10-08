0a4cfbe1227823042cc9533e62c4e16dfe465559
swaaws <40453962+swaaws@users.noreply.github.com>
2019-10-09 00:16:18 +0200

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -37,11 +37,12 @@ cd ~
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+
 # Backup an existing .git folder... its stupid!
 if [ -d .git ]; then
     mkdir backup_dot_git
     mv .git backup_dot_git
-    tar -cf backup_dot_git.tar backup_dot_git
+    tar -cf backup_dot_git_$(date +%Y-%m-%d_%H-%M-%S).tar backup_dot_git
     rm -rf backup_dot_git
     echo "Found .git in your Home folder... be careful with big files"
     echo ".git folder moved to backup_dot_git.tar"
@@ -69,7 +70,7 @@ if [ -d .zsh ]; then
     mv .zsh backup_zsh_vim/
 fi
 
-tar -cf backup_zsh_vim.tar backup_zsh_vim/
+tar -cf backup_zsh_vim_$(date +%Y-%m-%d_%H-%M-%S).tar backup_zsh_vim/
 rm -rf backup_zsh_vim
 
 # get new dotfiles
